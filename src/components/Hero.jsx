78da8b7a84d915ceb9941b130a79e047b50ba17f
Johan Karpantschof <johan931@gmail.com>
2022-01-13 23:21:28 +0100

    change h1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <div className="p-3 hero">
             <img className="d-inline-block float-start align-middle" style={{ marginRight: '0.5rem', marginBottom: '0.5rem' }} src={logo} height="auto" width="50px" alt="logo" />
-            <h1 className="align-middle">CA2A2 Startcode</h1>
+            <h1 className="align-middle">Trip Travellers</h1>
         </div>
     );
 }
